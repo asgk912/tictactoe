b500052a1b288239f2345093ae6e81b8aa9d6657
Albert <asgk912@gmail.com>
2020-03-11 14:34:00 -0700

    tested if methods that check boards to decide if win or lose is decided work

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ class TicTacToe {
     return [Array(3).fill(' '), Array(3).fill(' '), Array(3).fill(' ')];
   }
 
-  checkBoard(r, c) {
+  winOrLose(r, c) {
+    console.log(this.checkRow(r));
+    console.log(this.checkColumn(c));
+    console.log(this.checkMajorDiagonal());
+    console.log(this.checkMinorDiagonal());
     return (this.checkRow(r) || this.checkColumn(c) || this.checkMajorDiagonal() || this.checkMinorDiagonal())
   }
 
@@ -26,7 +30,7 @@ class TicTacToe {
 
   checkColumn(c) {
     for(var i=0; i<3; i++) {
-      if(this.board[i][c] !== this.turn) return farlse;
+      if(this.board[i][c] !== this.turn) return false;
     }
     return true;
   }
@@ -65,4 +69,54 @@ class TicTacToe {
 
 var game = new TicTacToe();
 
+
+/* Test if check methods work
+console.log('CHECK Row');
+game.board = [
+  [' ', ' ', 'X'],
+  ['O', 'O', 'O'],
+  [' ', 'X', ' ']
+]
 game.printBoard();
+game.turn = 'O';
+game.winOrLose(1,1);
+
+game.board = [
+  ['X', 'X', 'X'],
+  ['O', ' ', 'O'],
+  [' ', 'O', ' ']
+]
+game.printBoard();
+game.turn = 'X';
+game.winOrLose(0,2);
+
+console.log('CHECK: Column');
+game.board = [
+  [' ', 'O', 'X'],
+  [' ', 'O', 'X'],
+  [' ', 'O', ' ']
+]
+game.turn = 'O';
+game.printBoard();
+game.winOrLose(2,1);
+
+console.log('CHECK: Main Diagonal');
+game.board = [
+  ['O', ' ', 'X'],
+  [' ', 'O', 'X'],
+  [' ', ' ', 'O']
+]
+game.turn = 'O';
+game.printBoard();
+game.winOrLose(2,2);
+
+console.log('CHECK: Minor Diagonal');
+game.board = [
+  ['O', ' ', 'X'],
+  [' ', 'X', 'X'],
+  ['X', 'O', 'O']
+]
+game.turn = 'X';
+game.printBoard();
+game.winOrLose(0,2);
+*/
